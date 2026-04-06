ccd4cec76b1593ce1781c92f90a91cbe887297e5
Galih Aleanda <galihaleandaaa@gmail.com>
2026-04-06 16:07:27 +0700

    Remove Repository & Interface

--- app/Models/Pic.php
+++ app/Models/Pic.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pic extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'service_area_id',
+    ];
+
+    public function serviceArea() {
+        return $this->belongsTo(ServiceArea::class);
+    }
 }
--- app/Models/Sto.php
+++ app/Models/Sto.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Sto extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'name',
+        'sektor_id'
+    ];
+
+    public function sektor() {
+        return $this->belongsTo(Sektor::class);
+    }
 }
